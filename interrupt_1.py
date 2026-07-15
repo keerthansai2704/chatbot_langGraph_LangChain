f635bed9ea4399144bbc039f93dcdf9224e35dcd
parupally keerthan sai <126911442+keerthansai2704@users.noreply.github.com>
2026-07-15 18:56:58 +0000

    interrupt++

--- interrupt_1.py
+++ interrupt_1.py
@@ -1,5 +1,14 @@
 from typing_extensions import TypedDict
 from langgraph.types import interrupt
+from langgraph.checkpoint.memory import MemorySaver
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph ,END,START
+
+load_dotenv()
+
+memory = MemorySaver()
+llm = init_chat_model("groq:llama-3.3-70b-versatile")
 
 class ApproveState(TypedDict):
     username:str
@@ -10,4 +19,41 @@ def approval_node(state:ApproveState):
         f"approved login for {state['username']}"
     )
     print("login approved")
-    return {}
+    return {
+
+    }
+builder  =StateGraph(ApproveState)
+
+builder.add_node("approve",approval_node)
+
+builder.add_edge(START,"approve")
+
+builder.add_edge("approve",END)
+
+graph=builder.compile(
+
+    checkpointer=memory
+)
+
+config = {
+    "configurable":{
+        "thread_id":"thread_1"
+    }
+}
+for event in graph.stream(
+    {
+        "username":"username"
+    },
+    config=config,
+    stream_mode="updates"
+):
+    print(event)
+    
+
+
+
+
+
+
+
+
